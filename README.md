dce186e3b9628c979317814f114422b8da95baa1
Cléber Zavadniak <cleber@dronemapp.com>
2018-12-24 16:39:05 -0200

    First commit. Working fine on my MX Linux.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # wifi.sh
-A very very minimalistic script to connect your Linux box to a WPA2 access point
+
+A very very minimalistic script to connect your Linux box to a WPA2 access
+point.
+
+**You must run this app as root!**
+
+## Dependencies:
+
+* `iw`
+* `dhcpcd`
+* probably, `firmware-linux`
+
+### Install on debian-based distros
+
+```bash
+$ sudo apt-get install iw dhcpcd
+$ sudo apt-get install firmware-linux  # If your wireless card needs it.
+```
+
+## Troubleshooting
+
+### "It is not working!"
+
+1- This app only works with WPA/WPA2 access points.
+
+2- You must have root privileges.
+
+3- Your system can't have NetworkManager service running (`service
+network-manager stop` or whatever command your distro uses).
+
+4- Read the source code (`cat wifi.sh`), understand what it does, research
+a little about wireless connections and solve your problem yourself.
+
+5- If nothing else works, install NetworkManager or wicd. Both kind of
+sucks, but they use to work well enough for most cases.
---
+++ wifi.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+cmd_basename=$(basename $0)
+
+if [[ $1 == "" || $1 == "-h" || $1 == "--help" || $1 == "help" ]];then
+    interface=NONE
+    command="help"
+elif [[ $2 == "" ]];then
+    echo "Usage: $cmd_basename interface-name command [args]"
+    exit 1
+else
+    interface=$1
+    command=$2
+    shift 2
+fi
+
+
+function list () {
+    iw $interface scan | egrep '(SSID|)' --color
+}
+
+function enter_password () {
+    ssid=$1
+    ssid_path="/tmp/wpa.$(echo $ssid | sed 's:[^a-zA-Z0-9]:-:g')"
+
+    if [[ -f $ssid_path ]];then
+        echo "$ssid_path exists. Using it" >&2
+        echo "To re-enter this AP passphrase, remove this file" >&2
+    else
+        echo -n "Enter passphrase: " >&2
+        read passphrase
+        echo $passphrase | wpa_passphrase $ssid > $ssid_path
+    fi
+    chmod og-rwx $ssid_path
+    echo $ssid_path
+}
+
+function connect () {
+    ssid=$1
+    ssid_path=$(enter_password $ssid)
+    dhcpcd $interface
+    wpa_supplicant -i $interface -c $ssid_path
+}
+
+function help () {
+    echo "Usage:"
+    echo "    $cmd_basename <interface> list"
+    echo "    $cmd_basename <interface> connect <ap_ssid>"
+    echo
+    echo "Use ctrl+c to kill the connection"
+    echo
+    echo "Examples:"
+    echo "    $cmd_basename wlan0 list | less  # You can then search for 'SSID'"
+    echo "    $cmd_basename wlan0 connect homewifi_XYZ"
+}
+
+$command $*
